202e2d5af210b3c1ccbefe70741229f4b99f0f3d
TerliChaitanya <chaitutherli123@gmail.com>
2025-08-20 23:58:10 +0530

    Added LeetCode Java solutions

---
+++ prblm_283.java
@@ -0,0 +1,12 @@
+void moveZeroes(int* nums, int n) {
+    int j=0;
+        
+        for(int i=0;i<n;i++){
+            if(nums[i]!=0){
+              int temp=nums[i];
+              nums[i]=nums[j];
+              nums[j]=temp;
+              j++;
+            }
+        }
+}
