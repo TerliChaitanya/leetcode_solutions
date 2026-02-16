617c36bbd79a8d8629cd5a1cebb51bf35f9b0a58
TerliChaitanya <chaitutherli123@gmail.com>
2026-02-16 20:22:29 +0530

    Added solution for leetcode problem isomorphic Strings

---
+++ prblm_205.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        HashMap<Character,Character> hm=new HashMap<>();
+        HashMap<Character,Character> hm1=new HashMap<>();
+        for(int i=0;i<s.length();i++)
+        {
+            if(hm.containsKey(s.charAt(i)))
+            {
+                if(hm.get(s.charAt(i))!=t.charAt(i))
+                {
+                    return false;
+                }
+            }
+            else{
+                    hm.put(s.charAt(i),t.charAt(i));
+            }
+            if(hm1.containsKey(t.charAt(i)))
+            {
+                if(hm1.get(t.charAt(i))!=s.charAt(i))
+                {
+                    return false;
+                }
+            }
+            else{
+                    hm1.put(t.charAt(i),s.charAt(i));
+            }
+            
+        }
+        return true;
+    }
+}
